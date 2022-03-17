e9d767ce6f383079c894f43d64c7dcd75940da22
Emilien <emilien.billy@gmail.com>
2022-03-17 15:48:31 +0100

    Installation de Supabase et création de Client.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Metiers from "./Metiers";
+
+function App() {
+    return (
+        <div className="App">
+            <Metiers />
+        </div>
+    );
+}
+
+export default App;
