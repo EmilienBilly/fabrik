4f14ee9c25a078f3739f01b2d607644197c81ade
Emilien <emilien.billy@gmail.com>
2022-04-07 18:08:16 +0200

    Suppression du rendu conditionnel avec opérateur et ternaire, remplacé par la fonction handleColor avec switch et le props categorie en paramètre

--- src/JobDetails.js
+++ src/JobDetails.js
@@ -5,11 +5,30 @@ import styled from "styled-components";
 
 const colors = {
     alimentation: "linear-gradient(90deg, #4DAA57 0%, #8af27a 100%);",
-    hotellerie: "linear-gradient( 109.6deg,  rgba(45,116,213,1) 11.2%, rgba(121,137,212,1) 91.2% );",
+    restauration: "linear-gradient( 109.6deg,  rgba(45,116,213,1) 11.2%, rgba(121,137,212,1) 91.2% );",
     batiment: "linear-gradient( 69.7deg,  rgba(216,81,82,1) 40%, rgba(154,27,69,1) 100.1% );",
-    commerce: "radial-gradient( circle farthest-corner at 10% 20%,  rgba(174,24,255,1) 0%, rgba(246,134,255,1) 90% );",
+    services: "radial-gradient( circle farthest-corner at 10% 20%,  rgba(174,24,255,1) 0%, rgba(246,134,255,1) 90% );",
     metaux: "radial-gradient( circle farthest-corner at 10% 20%,  rgba(97,186,255,1) 0%, rgba(166,239,253,1) 90.1% );",
     mecanique: "radial-gradient( circle farthest-corner at 10% 20%,  rgba(255,209,67,1) 0%, rgba(255,145,83,1) 90% );",
+};
+
+const handleColor = (categorie) => {
+    switch (categorie) {
+        case "alimentation":
+            return colors.alimentation;
+        case "restauration":
+            return colors.restauration;
+        case "batiment":
+            return colors.batiment;
+        case "services":
+            return colors.services;
+        case "metaux":
+            return colors.metaux;
+        case "mecanique":
+            return colors.mecanique;
+        default:
+            return "";
+    }
 };
 
 const StyledJobDetails = styled.div`
@@ -17,20 +36,10 @@ const StyledJobDetails = styled.div`
 `;
 
 const StyleJobTitle = styled.h2`
-    background-image: ${({ categorie }) =>
-        categorie === "alimentation"
-            ? colors.alimentation
-            : categorie === "hotellerie"
-            ? colors.hotellerie
-            : categorie === "batiment"
-            ? colors.batiment
-            : categorie === "commerce"
-            ? colors.commerce
-            : categorie === "metaux"
-            ? colors.metaux
-            : categorie === "mecanique"
-            ? colors.mecanique
-            : "black"};
+    background-image: ${({ categorie }) => handleColor(categorie)};
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
 `;
 
 const JobDetails = () => {
@@ -48,17 +57,18 @@ const JobDetails = () => {
     }
 
     return (
-        <StyledJobDetails>
+        <div>
             {/* Conditional templating to avoid TypeError */}
             {job && (
-                <div>
-                    <StyleJobTitle categorie={job.category}>{titre}</StyleJobTitle>
+                <StyledJobDetails>
                     <div>
+                        <StyleJobTitle categorie={job.category}>{titre}</StyleJobTitle>
                         <p>{job.description}</p>
                     </div>
-                </div>
+                    <div></div>
+                </StyledJobDetails>
             )}
-        </StyledJobDetails>
+        </div>
     );
 };
 
